e85d49f9daf1c826ba12d182f6079d6f4eb80719
StankkyPete <Peter@Peters-MacBook-Air.local>
2014-12-28 16:43:19 -0500

    Initial Commit

---
+++ CrystalBall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrystalBall
+//
+//  Created by Peter Dorsaneo on 12/28/14.
+//  Copyright (c) 2014 Peter Dorsaneo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrystalBall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrystalBall
+//
+//  Created by Peter Dorsaneo on 12/28/14.
+//  Copyright (c) 2014 Peter Dorsaneo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
